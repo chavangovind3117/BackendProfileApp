bda841b04df47c89fc52145acc8d31ef8b153f9c
chavangovind3117 <chavangovind3117@gmail.com>
2025-01-08 23:53:13 +0530

    add port into env file

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 // Sample JSON data for users and salary details
 const users = [
     {
@@ -340,20 +342,6 @@ app.get('/fetch-salary', (req, res) => {
 
 // **-- Start the server --**
 
-const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-// ******-- Login endpoint --******
-
-// app.post('/login', (req, res) => {
-//     const { username, password } = req.body;
-//     const user = users.find((u) => u.username === username && u.password === password);
-
-//     if (user) {
-//         res.json({ success: true });
-//     } else {
-//         res.json({ success: false });
-//     }
-// });
