5babb46a4f6ea0f222de11516c2ff4f0011786eb
chavangovind3117 <chavangovind3117@gmail.com>
2025-01-14 19:11:46 +0530

    update db url

--- database/database.jsx
+++ database/database.jsx
@@ -2,12 +2,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 // Create a MySQL connection pool
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+// });
+
+const pool = mysql.createPool(process.env.DB_URL);
 
 // Testing the database connection
 (async () => {
