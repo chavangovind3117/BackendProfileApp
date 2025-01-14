b3dc9150749ff5ca3383faf695eb2f5ffbf84bf8
chavangovind3117 <chavangovind3117@gmail.com>
2025-01-14 19:23:49 +0530

    update env

--- database/database.jsx
+++ database/database.jsx
@@ -8,8 +8,14 @@ require('dotenv').config();
 //     password: process.env.DB_PASS,
 //     database: process.env.DB_NAME,
 // });
-
-const pool = mysql.createPool(process.env.DB_URL);
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,        // MySQL host (e.g., 'localhost' or 'db.example.com')
+    user: process.env.DB_USER,        // MySQL username
+    password: process.env.DB_PASS, // MySQL password
+    database: process.env.DB_NAME,    // MySQL database name
+    port: process.env.DB_PORT || 3306 // MySQL port (default is 3306)
+});
+// const pool = mysql.createPool(process.env.DB_URL);
 
 // Testing the database connection
 (async () => {
